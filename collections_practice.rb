fbe6e9d2dc2062d615edde4148957dc218eca3c5
Zachary Lehmann <zlehmann@gmail.com>
2018-09-15 23:11:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(ints)
+  ints.sort()
+end
 
+def sort_array_desc(array)
+  array.sort! {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  second = array[1]
+  third = array[2]
+  array[1] = third
+  array[2] = second
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect { |element, index|
+    if index == 1
+      element
+    else
+      "#{element}s"
+    end
+  }
+end
