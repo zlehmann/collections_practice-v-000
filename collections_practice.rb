a6eff7b79bd54f5bcc6097ff8ef1f5d377dbea55
Zachary Lehmann <zlehmann@gmail.com>
2018-09-15 22:51:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,11 @@ end
 def sort_array_char_count(array)
   array.sort {|x,y| x.length <=> y.length}
 end
+
+def swap_elements(array)
+  second = array[1]
+  third = array[2]
+  array[1] = third
+  array[2] = second
+  return array
+end
